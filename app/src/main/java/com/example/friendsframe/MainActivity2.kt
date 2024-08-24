97f7ef0202725357e9767eb1829fb51cd818cb31
Waris Khan <topupid1976@gmail.com>
2024-08-25 04:04:56 +0530

    Hey Its My First Project

---
+++ app/src/main/java/com/example/friendsframe/MainActivity2.kt
@@ -0,0 +1,29 @@
+package com.example.friendsframe
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.storage.UploadTask
+
+class MainActivity2 : AppCompatActivity() {
+
+    var uploadImage = ImageView;
+    final private Fire
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        uploadImage = findViewById<ImageView>(R.id.uploadImage)
+    }
+}
---
+++ app/src/main/java/com/example/friendsframe/dataClass.java
@@ -0,0 +1,23 @@
+package com.example.friendsframe;
+
+public class dataClass {
+    private  String Image
+
+
+    public dataClass(){
+
+
+    }
+
+    public void setImage(String image) {
+        Image = image;
+    }
+
+    public String getImage() {
+        return Image;
+    }
+
+    public dataClass(String image) {
+        Image = image;
+    }
+}
